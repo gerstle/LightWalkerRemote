d80f3707097d5ec2fc9cc732664ad9ae43a19190
casey gerstle <gerstle@gmail.com>
2018-11-06 11:06:52 -0800

    formatting

--- app/src/main/java/com/inappropirates/lightwalker/ui/ColorPickerPreference.java
+++ app/src/main/java/com/inappropirates/lightwalker/ui/ColorPickerPreference.java
@@ -20,217 +20,156 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ColorPickerPreference extends DialogPreference implements OnClickListener {
-	private int mColor = Color.BLACK;
-	private TextView mSplashText,mValueText;
+    private int mColor = Color.BLACK;
     private Context mContext;
 
-    private String mDialogMessage;
-	
-	public ColorPickerPreference(Context context, AttributeSet attrs) {
-		super(context, attrs);
+    public ColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-		mContext = context;
-	}
+        mContext = context;
+    }
 
-	/**
-	 * Interface describing a color change listener.
-	 */
-	public interface OnColorChangedListener {
-		/**
-		 * Method colorChanged is called when a new color is selected.
-		 * @param color new color.
-		 */
-		void colorChanged(int color);
-	}
+    private class ColorPickerView extends View {
+        private int CENTER_X = 400;
+        private int CENTER_Y = 400;
+        private int CENTER_RADIUS = 50;
 
-	/**
-	 * Inner class representing the color chooser.
-	 */
-	private class ColorPickerView extends View {
-		private int CENTER_X = 400;
-		private int CENTER_Y = 400;
-		private int CENTER_RADIUS = 50;
+        private Paint paint;
+        private Paint mCenterPaint;
+        private final int[] mColors = new int[]{
+                0xFFFF0000, 0xFFFF00FF, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
+                0xFF00FFFF, 0xFF00FF00, 0xFFFFFF00, 0xFFFF0000};
 
-		private Paint paint = null;
-		private Paint mCenterPaint = null;
-		private final int[] mColors = new int[] {
-			0xFFFF0000, 0xFFFF00FF, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-			0xFF00FFFF, 0xFF00FF00,	0xFFFFFF00, 0xFFFF0000 };
+        ColorPickerView(Context context, int initialColor) {
+            super(context);
 
-		ColorPickerView(Context context, int initialColor) {
-			super(context);
+            mColor = initialColor;
+            paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setShader(new SweepGradient(0, 0, mColors, null));
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(300);
 
-			mColor = initialColor;
-			paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-			paint.setShader(new SweepGradient(0, 0, mColors, null));
-			paint.setStyle(Paint.Style.STROKE);
-			paint.setStrokeWidth(300);
+            mCenterPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            mCenterPaint.setColor(mColor);
+            mCenterPaint.setStrokeWidth(5);
+        }
 
-			mCenterPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-			mCenterPaint.setColor(mColor);
-			mCenterPaint.setStrokeWidth(5);
-		}
-		
-        private boolean mTrackingCenter;
-        private boolean mHighlightCenter;
+        @SuppressLint("DrawAllocation")
+        protected void onDraw(Canvas canvas) {
+            int centerX = getRootView().getWidth() / 2 -
+                    (int) (paint.getStrokeWidth() / 4);
+            float r = CENTER_X - paint.getStrokeWidth() / 2;
 
-		/**
-		 * {@inheritDoc}
-		 */
-		@SuppressLint("DrawAllocation")
-		protected void onDraw(Canvas canvas) {
-			int centerX = getRootView().getWidth()/2 -
-				(int)(paint.getStrokeWidth()/4);
-			float r = CENTER_X - paint.getStrokeWidth()/2;
+            canvas.translate(centerX, CENTER_Y);
+            canvas.drawOval(new RectF(-r, -r, r, r), paint);
+            canvas.drawCircle(0, 0, CENTER_RADIUS, mCenterPaint);
+        }
 
-			canvas.translate(centerX, CENTER_Y);
-			canvas.drawOval(new RectF(-r, -r, r, r), paint);
-			canvas.drawCircle(0, 0, CENTER_RADIUS, mCenterPaint);
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            int width = getRootView().getWidth();
 
-			if (mTrackingCenter) {
-				int c = mCenterPaint.getColor();
-				mCenterPaint.setStyle(Paint.Style.STROKE);
-				
-				 if (mHighlightCenter) {
-	                 mCenterPaint.setAlpha(0xFF);
-	             } else {
-	            	 mCenterPaint.setAlpha(0x80);
-	             }
+            if (width == 0) {
+                width = CENTER_X * 2 + 50;
+            }
 
-				canvas.drawCircle(0, 0, CENTER_RADIUS +
-					mCenterPaint.getStrokeWidth(), mCenterPaint);
+            setMeasuredDimension(width, CENTER_Y * 2);
+        }
 
-				mCenterPaint.setStyle(Paint.Style.FILL);
-				mCenterPaint.setColor(c);
-			}
-		}
-		
-		/**
-		 * {@inheritDoc}
-		 */
-		protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-			int width = getRootView().getWidth();
+        private int ave(int s, int d, float p) {
+            return s + Math.round(p * (d - s));
+        }
 
-			if (width == 0) {
-				width = CENTER_X*2 + 50;
-			}
+        private int interpColor(int mColors[], float unit) {
+            if (unit <= 0) {
+                return mColors[0];
+            }
 
-			setMeasuredDimension(width, CENTER_Y * 2);
-		}
-		/**
-		 * @param s
-		 * @param d
-		 * @param p
-		 * @return
-		 */
-		private int ave(int s, int d, float p) {
-			return s + Math.round(p * (d - s));
-		}
-		/**
-		 * @param mColors
-		 * @param unit
-		 * @return
-		 */
-		private int interpColor(int mColors[], float unit) {
-			if (unit <= 0) {
-				return mColors[0];
-			}
+            if (unit >= 1) {
+                return mColors[mColors.length - 1];
+            }
 
-			if (unit >= 1) {
-				return mColors[mColors.length - 1];
-			}
+            float p = unit * (mColors.length - 1);
+            int i = (int) p;
+            p -= i;
 
-			float p = unit * (mColors.length - 1);
-			int i = (int) p;
-			p -= i;
+            // now p is just the fractional part [0...1) and i is the index
+            int c0 = mColors[i];
+            int c1 = mColors[i + 1];
+            int a = ave(Color.alpha(c0), Color.alpha(c1), p);
+            int r = ave(Color.red(c0), Color.red(c1), p);
+            int g = ave(Color.green(c0), Color.green(c1), p);
+            int b = ave(Color.blue(c0), Color.blue(c1), p);
 
-			// now p is just the fractional part [0...1) and i is the index
-			int c0 = mColors[i];
-			int c1 = mColors[i + 1];
-			int a = ave(Color.alpha(c0), Color.alpha(c1), p);
-			int r = ave(Color.red(c0), Color.red(c1), p);
-			int g = ave(Color.green(c0), Color.green(c1), p);
-			int b = ave(Color.blue(c0), Color.blue(c1), p);
+            return Color.argb(a, r, g, b);
+        }
 
-			return Color.argb(a, r, g, b);
-		}
-		
         private static final float PI = 3.1415926f;
 
-		/**
-		 * {@inheritDoc}
-		 */
-		public boolean onTouchEvent(MotionEvent event) {
-			float x = event.getX() - getRootView().getWidth()/2;
-			float y = event.getY() - CENTER_Y;
-            boolean inCenter = java.lang.Math.sqrt(x*x + y*y) <= CENTER_RADIUS;
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX() - getRootView().getWidth() / 2;
+            float y = event.getY() - CENTER_Y;
+            boolean inCenter = java.lang.Math.sqrt(x * x + y * y) <= CENTER_RADIUS;
 
-			switch (event.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-			case MotionEvent.ACTION_MOVE:
-				if (!inCenter)
-				{
-                    float angle = (float)java.lang.Math.atan2(y, x);
-                    // need to turn angle [-PI ... PI] into unit [0....1]
-                    float unit = angle/(2*PI);
-                    if (unit < 0) {
-                        unit += 1;
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_MOVE:
+                    if (!inCenter) {
+                        float angle = (float) java.lang.Math.atan2(y, x);
+                        // need to turn angle [-PI ... PI] into unit [0....1]
+                        float unit = angle / (2 * PI);
+                        if (unit < 0) {
+                            unit += 1;
+                        }
+                        mCenterPaint.setColor(interpColor(mColors, unit));
+                        invalidate();
                     }
-                    mCenterPaint.setColor(interpColor(mColors, unit));
-                    invalidate();
-				}
-                break;
+                    break;
 
-			case MotionEvent.ACTION_UP:
-				mColor = mCenterPaint.getColor();
-                break;
-			}
+                case MotionEvent.ACTION_UP:
+                    mColor = mCenterPaint.getColor();
+                    break;
+            }
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 
     @Override
-    protected void onSetInitialValue(boolean restore, Object defaultValue)  
-    {
+    protected void onSetInitialValue(boolean restore, Object defaultValue) {
         super.onSetInitialValue(restore, defaultValue);
-        if (restore) 
+        if (restore)
             mColor = shouldPersist() ? getPersistedInt(Color.BLACK) : Color.BLACK;
-        else 
+        else
             mColor = Color.BLACK;
     }
-	
-	protected View onCreateDialogView()
-	{
+
+    protected View onCreateDialogView() {
         LinearLayout.LayoutParams params;
         LinearLayout layout = new LinearLayout(mContext);
         layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setPadding(6,6,6,6);
+        layout.setPadding(6, 6, 6, 6);
 
-        mSplashText = new TextView(mContext);
-        if (mDialogMessage != null)
-            mSplashText.setText(mDialogMessage);
+        TextView mSplashText = new TextView(mContext);
         layout.addView(mSplashText);
 
-        mValueText = new TextView(mContext);
+        TextView mValueText = new TextView(mContext);
         mValueText.setGravity(Gravity.CENTER_HORIZONTAL);
         mValueText.setTextSize(32);
         params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 
+                LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
         layout.addView(mValueText, params);
-        
+
         if (shouldPersist())
             mColor = getPersistedInt(Color.BLACK);
 
         ColorPickerView mColorPicker = new ColorPickerView(getContext(), mColor);
-		layout.addView(mColorPicker, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-
+        layout.addView(mColorPicker, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
 
         return layout;
-	}
-	
+    }
+
     // ------------------------------------------------------------------------------------------
     // Set the positive button listener and onClick action : 
     @Override
@@ -247,9 +186,9 @@ public class ColorPickerPreference extends DialogPreference implements OnClickLi
 
         if (shouldPersist())
             persistInt(mColor);
-        callChangeListener(Integer.valueOf(mColor));
+        callChangeListener(mColor);
 
-        ((AlertDialog) getDialog()).dismiss();
+        getDialog().dismiss();
     }
     // ------------------------------------------------------------------------------------------
 }
--- app/src/main/java/com/inappropirates/lightwalker/ui/SeekBarPreference.java
+++ app/src/main/java/com/inappropirates/lightwalker/ui/SeekBarPreference.java
@@ -1,10 +1,10 @@
-/* The following code was written by Matthew Wiggins 
- * and is released under the APACHE 2.0 license 
- * 
+/* The following code was written by Matthew Wiggins
+ * and is released under the APACHE 2.0 license
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Improvements :
- * - save the value on positive button click, not on seekbar change
+ * - save the value on positive button click, not on seek bar change
  * - handle @string/... values in xml file
  */
 
@@ -24,14 +24,13 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 
-public class SeekBarPreference extends DialogPreference implements SeekBar.OnSeekBarChangeListener, OnClickListener
-{
+public class SeekBarPreference extends DialogPreference implements SeekBar.OnSeekBarChangeListener, OnClickListener {
     // ------------------------------------------------------------------------------------------
     // Private attributes :
-    private static final String androidns="http://schemas.android.com/apk/res/android";
+    private static final String androidns = "http://schemas.android.com/apk/res/android";
 
     private SeekBar mSeekBar;
-    private TextView mSplashText,mValueText;
+    private TextView mValueText;
     private Context mContext;
 
     private String mDialogMessage, mSuffix;
@@ -39,43 +38,42 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
     // ------------------------------------------------------------------------------------------
 
 
-
     // ------------------------------------------------------------------------------------------
     // Constructor :
     public SeekBarPreference(Context context, AttributeSet attrs) {
 
-        super(context,attrs); 
+        super(context, attrs);
         mContext = context;
 
         // Get string value for dialogMessage :
         int mDialogMessageId = attrs.getAttributeResourceValue(androidns, "dialogMessage", 0);
-        if(mDialogMessageId == 0) mDialogMessage = attrs.getAttributeValue(androidns, "dialogMessage");
+        if (mDialogMessageId == 0)
+            mDialogMessage = attrs.getAttributeValue(androidns, "dialogMessage");
         else mDialogMessage = mContext.getString(mDialogMessageId);
 
         // Get string value for suffix (text attribute in xml file) :
         int mSuffixId = attrs.getAttributeResourceValue(androidns, "text", 0);
-        if(mSuffixId == 0) mSuffix = attrs.getAttributeValue(androidns, "text");
+        if (mSuffixId == 0) mSuffix = attrs.getAttributeValue(androidns, "text");
         else mSuffix = mContext.getString(mSuffixId);
 
-        // Get default and max seekbar values :
+        // Get default and max seek bar values :
         mDefault = attrs.getAttributeIntValue(androidns, "defaultValue", 0);
         mMax = attrs.getAttributeIntValue(androidns, "max", 100);
     }
     // ------------------------------------------------------------------------------------------
 
 
-
     // ------------------------------------------------------------------------------------------
     // DialogPreference methods :
-    @Override 
+    @Override
     protected View onCreateDialogView() {
 
         LinearLayout.LayoutParams params;
         LinearLayout layout = new LinearLayout(mContext);
         layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setPadding(6,6,6,6);
+        layout.setPadding(6, 6, 6, 6);
 
-        mSplashText = new TextView(mContext);
+        TextView mSplashText = new TextView(mContext);
         if (mDialogMessage != null)
             mSplashText.setText(mDialogMessage);
         layout.addView(mSplashText);
@@ -84,7 +82,7 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
         mValueText.setGravity(Gravity.CENTER_HORIZONTAL);
         mValueText.setTextSize(32);
         params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 
+                LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
         layout.addView(mValueText, params);
 
@@ -101,7 +99,7 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
         return layout;
     }
 
-    @Override 
+    @Override
     protected void onBindDialogView(View v) {
         super.onBindDialogView(v);
         mSeekBar.setMax(mMax);
@@ -109,43 +107,50 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
     }
 
     @Override
-    protected void onSetInitialValue(boolean restore, Object defaultValue)  
-    {
+    protected void onSetInitialValue(boolean restore, Object defaultValue) {
         super.onSetInitialValue(restore, defaultValue);
         if (restore)
-        	mValue = shouldPersist() ? getPersistedInt(mDefault) : 0;
-        else 
-            mValue = (Integer)defaultValue;
+            mValue = shouldPersist() ? getPersistedInt(mDefault) : 0;
+        else
+            mValue = (Integer) defaultValue;
     }
     // ------------------------------------------------------------------------------------------
-
 
 
     // ------------------------------------------------------------------------------------------
     // OnSeekBarChangeListener methods :
     @Override
-    public void onProgressChanged(SeekBar seek, int value, boolean fromTouch)
-    {
+    public void onProgressChanged(SeekBar seek, int value, boolean fromTouch) {
         String t = String.valueOf(value);
         mValueText.setText(mSuffix == null ? t : t.concat(" " + mSuffix));
     }
 
     @Override
-    public void onStartTrackingTouch(SeekBar seek) {}
+    public void onStartTrackingTouch(SeekBar seek) {
+    }
+
     @Override
-    public void onStopTrackingTouch(SeekBar seek) {}
+    public void onStopTrackingTouch(SeekBar seek) {
+    }
 
-    public void setMax(int max) { mMax = max; }
-    public int getMax() { return mMax; }
+    public void setMax(int max) {
+        mMax = max;
+    }
 
-    public void setProgress(int progress) { 
+    public int getMax() {
+        return mMax;
+    }
+
+    public void setProgress(int progress) {
         mValue = progress;
         if (mSeekBar != null)
-            mSeekBar.setProgress(progress); 
+            mSeekBar.setProgress(progress);
     }
-    public int getProgress() { return mValue; }
-    // ------------------------------------------------------------------------------------------
 
+    public int getProgress() {
+        return mValue;
+    }
+    // ------------------------------------------------------------------------------------------
 
 
     // ------------------------------------------------------------------------------------------
